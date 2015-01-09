2fbfafe580da22e597fabc7890dd0b3fd466a55e
Kenny Darrell <darrelkj@gmail.com>
2015-01-08 22:20:00 -0500

    package imports stringr, calls not needed

--- R/date_utils.R
+++ R/date_utils.R
@@ -1,29 +1,29 @@
-format_timestamp <- function(indf, index=1) {
+format_timestamp <- function(indf, index = 1) {
   if (class(indf[[index]])[1] == "POSIXlt") {
     return(indf)
   }
-  if (stringr::str_detect(indf[[index]][1], "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} \\+\\d{4}$")) {
+  if (str_detect(indf[[index]][1], "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} \\+\\d{4}$")) {
     indf[[index]] <- strptime(indf[[index]], format="%Y-%m-%d %H:%M:%S", tz="UTC")
   }
-  else if (stringr::str_detect(indf[[index]][1], "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$")) {
+  else if (str_detect(indf[[index]][1], "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$")) {
     indf[[index]] <- strptime(indf[[index]], format="%Y-%m-%d %H:%M:%S", tz="UTC")
   }
-  else if (stringr::str_detect(indf[[index]][1], "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}$")) {
+  else if (str_detect(indf[[index]][1], "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}$")) {
     indf[[index]] <- strptime(indf[[index]], format="%Y-%m-%d %H:%M", tz="UTC")
   }
-  else if (stringr::str_detect(indf[[index]][1], "^\\d{2}/\\d{2}/\\d{2}$")) {
+  else if (str_detect(indf[[index]][1], "^\\d{2}/\\d{2}/\\d{2}$")) {
     indf[[index]] <- strptime(indf[[index]], format="%m/%d/%y", tz="UTC")
   }
-  else if (stringr::str_detect(indf[[index]][1], "^\\d{2}/\\d{2}/\\d{4}$")) {
+  else if (str_detect(indf[[index]][1], "^\\d{2}/\\d{2}/\\d{4}$")) {
     indf[[index]] <- strptime(indf[[index]], format="%m/%d/%Y", tz="UTC")
   }
-  else if (stringr::str_detect(indf[[index]][1], "^\\d{4}\\d{2}\\d{2}$")) {
+  else if (str_detect(indf[[index]][1], "^\\d{4}\\d{2}\\d{2}$")) {
     indf[[index]] <- strptime(indf[[index]], format="%Y%m%d", tz="UTC")
   }
-  else if (stringr::str_detect(indf[[index]][1], "^\\d{4}/\\d{2}/\\d{2}/\\d{2}$")) {
+  else if (str_detect(indf[[index]][1], "^\\d{4}/\\d{2}/\\d{2}/\\d{2}$")) {
     indf[[index]] <- strptime(indf[[index]], format="%Y/%m/%d/%H", tz="UTC")
   }
-  else if (stringr::str_detect(indf[[index]][1], "^\\d{10}$")) {
+  else if (str_detect(indf[[index]][1], "^\\d{10}$")) {
     # Handle Unix seconds in milliseconds
     indf[[index]] <- as.POSIXlt(indf[[index]], origin="1970-01-01", tz="UTC")
   }
