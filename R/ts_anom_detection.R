52192a3b01af5ead6ff1fe46711beeabf94c2404
Kenny Darrell <darrelkj@gmail.com>
2015-01-08 22:40:14 -0500

    remove list name quotes

--- R/ts_anom_detection.R
+++ R/ts_anom_detection.R
@@ -325,8 +325,8 @@ AnomalyDetectionTs <- function(x, max_anoms = 0.10, direction = 'pos',
   
   # Lastly, return anoms and optionally the plot if requested by the user
   if(plot){
-    return (list("anoms"=anoms, "plot"=xgraph))
+    return (list(anoms = anoms, plot = xgraph))
   } else {
-    return (list("anoms"=anoms, "plot"=NULL))
+    return (list(anoms = anoms, plot = NULL))
   }
 }
