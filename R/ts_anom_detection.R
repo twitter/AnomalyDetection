12f947cc9a1ae28f67f691baed5b7536f55409e3
owen vallis <owen@kadenze.com>
2015-04-15 17:19:30 -0700

    added 'stringsAsFactors=FALSE' to creation of data frames after fix for %Y-%m-%d %H:%M:%S formatting issue. Implements suggested patch for issue #29

--- R/ts_anom_detection.R
+++ R/ts_anom_detection.R
@@ -326,9 +326,11 @@ AnomalyDetectionTs <- function(x, max_anoms = 0.10, direction = 'pos',
   
   # Store expected values if set by user
   if(e_value) {
-    anoms <- data.frame(timestamp=all_anoms[[1]], anoms=all_anoms[[2]], expected_value=subset(seasonal_plus_trend[[2]], as.POSIXlt(seasonal_plus_trend[[1]], tz="UTC") %in% all_anoms[[1]]))
+    anoms <- data.frame(timestamp=all_anoms[[1]], anoms=all_anoms[[2]], 
+                        expected_value=subset(seasonal_plus_trend[[2]], as.POSIXlt(seasonal_plus_trend[[1]], tz="UTC") %in% all_anoms[[1]]),
+                        stringsAsFactors=FALSE)
   } else {
-    anoms <- data.frame(timestamp=all_anoms[[1]], anoms=all_anoms[[2]])
+    anoms <- data.frame(timestamp=all_anoms[[1]], anoms=all_anoms[[2]], stringsAsFactors=FALSE)
   }
 
   # Make sure we're still a valid POSIXlt datetime.
